34fee95815c6ad3958a2c5ecc2efd819941fc884
Francesco Balestrieri <bale@balenet.com>
2025-03-29 14:18:49 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Francesco Balestrieri on 29.3.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
